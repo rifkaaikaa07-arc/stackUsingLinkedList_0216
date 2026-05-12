40e0189e0f0a823cccc6a2d1f571d1955f518163
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-05-12 11:22:59 +0700

    menambahkan case 2

--- stackUsinglinkedlist.cpp
+++ stackUsinglinkedlist.cpp
@@ -97,6 +97,16 @@ int main()
             cin >> value;
             stack.push(value); // push the entered value into the stack
             break;
+        case 2:
+            if(!stack.isEmpty())
+            {
+                stack.pop(); // pop  the  top element from the stack
+            }
+            else
+            {
+                cout << "Stack is empty. Cannot pop." << endl;
+            }
+            break;
         }
     }
 };
