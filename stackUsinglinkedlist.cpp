eab0d16d114e73392964b6c756f5149da340ead3
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-05-12 11:15:24 +0700

    menambahkan int pada class stack

--- stackUsinglinkedlist.cpp
+++ stackUsinglinkedlist.cpp
@@ -25,4 +25,13 @@ class stack
     {
         top = NULL;
     }
+    int push(int value)
+    {
+        node*newnode = new node();
+        newnode->data = value;
+        newnode ->next = top;
+        top = newnode;
+        cout << "push value: " << value << endl;
+        return value;
+    }
 };
