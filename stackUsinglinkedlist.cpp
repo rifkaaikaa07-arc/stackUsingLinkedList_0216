4c3149efe11412c8d719150e9a11b351ee1cb007
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-05-12 11:18:43 +0700

    menambahkan bool pada class stack

--- stackUsinglinkedlist.cpp
+++ stackUsinglinkedlist.cpp
@@ -65,5 +65,11 @@ class stack
             }
             cout << endl;
         }
-    } 
+    } // return the value of the top node
+
+    // isEmpty operation: check of the stack is empty
+    bool isEmpty()
+    {
+        return top == NULL; //Return true if the top pointer is NULL, indicating an empty stack.
+    }
 };
